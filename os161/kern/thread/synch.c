79126c4d80f1c797298dac6fe04189c6ee2e43fe
sdmeaney <sdmeaney@gmail.com>
2018-02-15 04:15:01 -0800

    comment

--- os161/kern/thread/synch.c
+++ os161/kern/thread/synch.c
@@ -223,13 +223,13 @@ lock_acquire(struct lock *lock)
         // same time.
         //spinlock_acquire(&sem->sem_lock);
         // start our spinlock section
-        spinlock_acquire(&lock->lk_lock);
+ //       spinlock_acquire(&lock->lk_lock);
 
         //releasing our spinlock section
-        spinlock_release(&lock->lk_lock);
-            wchan_sleep(lock->lk_wchan);
+//        spinlock_release(&lock->lk_lock);
+//            wchan_sleep(lock->lk_wchan);
 
-            
+
         //end write this
 
         (void)lock;  // suppress warning until code gets written
