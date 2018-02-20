f6367f5c2731714c4d9849a30077c3447de60640
sdmeaney <sdmeaney@gmail.com>
2018-02-20 00:07:03 -0800

    checking if I'm actually the right one to release

--- os161/kern/thread/synch.c
+++ os161/kern/thread/synch.c
@@ -261,14 +261,18 @@ lock_release(struct lock *lock)
         //lock_release - Free the lock. Only the thread holding the lock may do
         //  this.
     KASSERT(lock != NULL);
-    spinlock_acquire(&lock->lk_lock); //protect since we have to mess with 
+    if(lock_do_i_hold(lock)){ //give our lock to lock do I hold to see if it's curthread
+        spinlock_acquire(&lock->lk_lock); //protect since we have to mess with 
+        wchan_wakeone(lock->lk_wchan); // let them all fight for the next slot
+        //lock->lock_locked = curthread; this is how we aquired
+        // just like in aquire, we release 
+        lock->lock_locked = NULL;
+        KASSERT(lock == NULL);
 
-    wchan_wakeone(lock->lk_wchan); // let them all fight for the next slot
-    //lock->lock_locked = curthread; this is how we aquired
-    // just like in aquire, we release 
-    lock->lock_locked = NULL;
-    spinlock_release(&lock->lk_lock); //end spinlock protect since we're done
-       // (void)lock;  // suppress warning until code gets written
+        spinlock_release(&lock->lk_lock); //end spinlock protect since we're done
+           // (void)lock;  // suppress warning until code gets written
+    }
+
 }
 
 bool
