aef2f3e6a2c16345df4184f0e3993f063c19903d
sdmeaney <sdmeaney@gmail.com>
2018-02-20 00:14:50 -0800

    more reverts

--- os161/kern/thread/synch.c
+++ os161/kern/thread/synch.c
@@ -265,11 +265,11 @@ lock_release(struct lock *lock)
 
   //  if(lock_do_i_hold(lock)){ //give our lock to lock do I hold to see if it's curthread
         spinlock_acquire(&lock->lk_lock); //protect since we have to mess with 
-        wchan_wakeone(lock->lk_wchan); // let them all fight for the next slot
+        wchan_wakeall(lock->lk_wchan); // let them all fight for the next slot
         //lock->lock_locked = curthread; this is how we aquired
         // just like in aquire, we release 
         lock->lock_locked = NULL;
-        KASSERT(lock == NULL);
+        //KASSERT(lock == NULL);
 
         spinlock_release(&lock->lk_lock); //end spinlock protect since we're done
            // (void)lock;  // suppress warning until code gets written
