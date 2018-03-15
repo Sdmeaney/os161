e6ef4c7c0dcac86faae8c8d80ec0d674b477c93c
sdmeaney <sdmeaney@gmail.com>
2018-03-15 03:34:54 -0700

    stub

--- os161/kern/syscall/proc_syscalls.c
+++ os161/kern/syscall/proc_syscalls.c
@@ -48,7 +48,12 @@ void sys__exit(int exitcode) {
   panic("return from thread_exit in sys_exit\n");
 }
 
-
+int
+sys_fork(pid_t, *retval)
+{
+  *retval = 2; //test
+  return 0; //test
+}
 /* stub handler for getpid() system call                */
 int
 sys_getpid(pid_t *retval)
