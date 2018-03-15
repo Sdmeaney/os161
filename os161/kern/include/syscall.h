f8900d22a0b9152215a27bcb3b83f37eb7956c6b
sdmeaney <sdmeaney@gmail.com>
2018-03-15 08:03:32 -0700

    header file oop

--- os161/kern/include/syscall.h
+++ os161/kern/include/syscall.h
@@ -43,8 +43,6 @@ void syscall(struct trapframe *tf);
  * Support functions.
  */
 
-/* Helper for fork(). You write this. */
-void enter_forked_process(struct trapframe *tf);
 
 /* Enter user mode. Does not return. */
 void enter_new_process(int argc, userptr_t argv, vaddr_t stackptr,
@@ -63,7 +61,7 @@ int sys_write(int fdesc,userptr_t ubuf,unsigned int nbytes,int *retval);
 void sys__exit(int exitcode);
 int sys_getpid(pid_t *retval);
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
-int sys_fork(pid_t *retval); //fork add
+int sys_fork(pid_t *retval,struct trapframe *passed_tf); //fork add
 void uproc_thread(void* temp_tf, unsigned long testvar);
 
 #endif // UW
