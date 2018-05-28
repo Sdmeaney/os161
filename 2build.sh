3c3475dffb0c064581dafe7a0b659e9d416c7dac
sdmeaney <sdmeaney@gmail.com>
2018-05-28 16:37:44 -0700

    oopsies

--- 2build.sh
+++ 2build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 git pull origin $(git rev-parse --abbrev-ref HEAD)
-pushd ~/os161/root
-
-sys161 kernel-ASST3
-
+pushd ~/os161/kern/conf
+./config ASST2
+cd ../compile/ASST3
+bmake depend
+bmake
+bmake install
 popd
