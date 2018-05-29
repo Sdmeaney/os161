08e8d5bed7358c334e0cc40836202296ea39d5dc
sdmeaney <sdmeaney@gmail.com>
2018-05-28 20:20:51 -0700

    debug

--- os161/kern/proc/proc.c
+++ os161/kern/proc/proc.c
@@ -133,7 +133,7 @@ void proctable_resize(void){
 }
 
 void proctable_remove(struct proc* p){
-		//kprintf("proc_remove\n");
+		kprintf("proc_remove\n");
 		lock_acquire(proc_table_mutex);
 		// look through the table to find if it has kids
 		for ( int i= 0; i < MAXARRAY; i++ ){
